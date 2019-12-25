9c44c5438b6d9360c6949605a4556d6cd9c9374b
Maksym Domin <maximd@ya0ne.com>
2019-12-25 22:00:09 +0200

    First pass

--- src/main/java/com/algorithms/sum_abc_max_d/Calculator.java
+++ src/main/java/com/algorithms/sum_abc_max_d/Calculator.java
@@ -1,5 +1,6 @@
 package com.algorithms.sum_abc_max_d;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.algorithms.common.generators.RandomListGenerator;
@@ -9,25 +10,69 @@ public class Calculator {
 	private final static long FROM_NUMBER = -100;
 	private final static long TO_NUMBER = 100;
 	private final static int NUMBERS = 100;
+	private List<?> list = new ArrayList<>();
+	
+	class PairAB {
+		private long A;
+		private long B;
 
-	public static void main(String[] args) {
-		RandomListGenerator randomListGenerator = new RandomListGenerator();
-		List<Long> list = randomListGenerator.getStreamOfRandomLongsWithRange(FROM_NUMBER, TO_NUMBER, NUMBERS);
-		//FileUtils.writeArray("sum.txt", list);
-		long latest1 = list.get(list.size()-1); // d, latest element
-		long latest2 = list.get(list.size()-2); // c, previous before latest
-		long leftSum = 0;
-		for(int i=0; i<list.size()-3; i++) {
-			leftSum = leftSum(list.get(i), list.get(i+1), latest1 - latest2);
-			latest1 = list.get(i);
-			latest2 = list.get(i-1);
+		PairAB(long a, long b) {
+			this.A = a;
+			this.B = b;
 		}
 	}
-	
-	private static long leftSum(long a, long b, long sum) {
-		while(a+b<=sum) {
-			leftSum(a, b, sum);
+
+	private static long D;
+
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		calculator.setList(RandomListGenerator.getStreamOfRandomLongsWithRange(NUMBERS, FROM_NUMBER, TO_NUMBER));
+		FileUtils.writeArray("sum.txt", calculator.getList());
+
+		D = calculator.calculateMaxD();
+		System.out.println(D);
+	}
+
+	public long calculateMaxD() {
+		List<Long> list = this.getList();
+
+		long D = list.get(list.size()-1); // d, latest max element
+		long C = list.get(list.size()-2); // c, previous before the latest max
+		long DminusC = D - C;
+		for(int i = list.size()-1; i >= 0; i--) {
+			PairAB AplusB = leftSum(0, DminusC);
+			if( AplusB != null ) {
+				return D;
+			}
+			D = list.get(i);
+			C = list.get(i-1);
+			DminusC = D-C;
 		}
-		return sum;
+		return D;
+	}
+
+	private PairAB leftSum(int a, long DminusC) {
+		List<Long> list = getList();
+		int i = a;
+		long AplusB = 0; //list.get(a) + list.get(b);
+		do {
+			AplusB = list.get(a) + list.get(i+1);
+			if(AplusB == DminusC) {
+				return new PairAB(list.get(a), list.get(i));
+			}
+			i++;
+		} while(i<list.size()-2 && list.get(i) < DminusC);
+		a++;
+		leftSum(a, DminusC);
+		return null;
+	}
+
+	public <T> void setList(List<T> list) {
+		this.list = list;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <T> List<T> getList() {
+		return (List<T>) this.list;
 	}
 }
---
+++ src/test/java/com/algorithms/sum_abc_max_d/CalculatorTest.java
@@ -0,0 +1,17 @@
+﻿package com.algorithms.sum_abc_max_d;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+	
+	private Calculator calculator = new Calculator();
+
+	@Test
+	public void shouldReturn12() {
+		calculator.setList(Arrays.asList(2L, 3L, 5L, 7L, 12L));
+		long D = calculator.calculateMaxD();
+		assertEquals(12, D);
+	}
+}
